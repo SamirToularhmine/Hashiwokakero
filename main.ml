21380c104e9dd42f53396c4e2f480913cabbcf9a
root <root@localhost.localdomain>
2019-03-30 00:57:56 +0100

    doucement mais surrement, définition de quelques operations pour travailer sur des puzzles

--- main.ml
+++ main.ml
@@ -9,6 +9,7 @@ type cell = Nothing | Island of importance | Bridge of bridge;;
 type solution = cell list list;;
 
 (* Puzzles *)
+(* Coordinate(colonne,ligne) *)
 let puzzle2 =
   Puzzle
     [
@@ -51,4 +52,39 @@ let rec getBords = fun p -> fun n ->
           else getBords (Puzzle t) n
        | _ -> []
      end;;
+(* fonctions pour travailler sur des coordinate s *)
+let fstcoord (Coordinate (x,y))=x;;
+let sndcoord (Coordinate (x,y))=y;;
 
+(* on definit des operateurs sur les coordinates  *)
+let (>~) c1 c2 = (fstcoord c1) > (fstcoord c2);;
+let (>~~) c1 c2 = (sndcoord c1) > (sndcoord c2);;
+(* petit test *)
+let _ = Coordinate (0,3) >~~ Coordinate (1,2);;
+(* ça marche bien *)
+
+
+
+(* on applique une fonction une paire de coordinnate... ah mais enfait elle sert à rien sert fonction *)
+let fonc_sur_coord f (x:coordinate) (y:coordinate) = f x y;; 
+
+(* recuperer c1 ou c2 selon un predicat *)
+let filtercoord f c1 c2 = fonc_sur_coord (fun x y ->if (f x y) then x else y) c1 c2;;
+
+(* max selon colonne et max selon ligne *)
+let maxfstcoord c1 c2 = filtercoord (>~) c1 c2;;
+let maxsndcoord c1 c2 = filtercoord (>~~) c1 c2;;
+
+(* operation pour travailler sur des puzzle s *)
+
+(* fonction de reduction sur des puzzle s *)
+let reducepuzzle f (pzl : puzzle) =
+  match pzl with
+  | Puzzle plist -> List.fold_right (f)  plist (List.nth plist 0);;
+
+(* petit test, ici on recupere la colonne max du puzzle *)
+let _ = fstcoord (fst(reducepuzzle ((fun x y->if (fst x) >~ (fst y) then x else y)) puzzle2));;
+
+(* nombre de (coordinate*importance) dans un puzzle  *)
+let lengthpzl (Puzzle l)=List.length l;;
+lengthpzl puzzle2;;
