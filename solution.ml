ac0f24fdcc44b9ae371931c2f6b6faa9f57c8f6c
Samir Toularhmine <samir.toularhmine@etu.univ-orleans.fr>
2019-04-07 16:13:06 +0200

    Ajout d'une fonction initSolution

--- solution.ml
+++ solution.ml
@@ -1,7 +1,10 @@
-open Puzzle;;
+open Puzzle
 
 type bridge = { isVertical : bool; isDoubled : bool}
 type cell = Nothing | Island of Puzzle.importance | Bridge of bridge
 type solution = cell list list
 
-let initSolution = fun p -> Puzzle.create p
+let initSolution = fun p ->
+  match Puzzle.list_of_puzzle p with
+  | [] -> []
+  | h::t -> [];;
--- solution.mli
+++ solution.mli
@@ -1,8 +1,8 @@
-open Puzzle;;
+open Puzzle
 
 type bridge
 type cell
 type solution
       
-val initSolution : (Coordinate.coordinate * Puzzle.importance) list -> Puzzle.puzzle
+val initSolution : Puzzle.puzzle -> solution
 
