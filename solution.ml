0f33faa5b3b19f4b7c429e9bc2144a2ce33d4abf
MahafalyRandriamiarisoa <mahafaly.randriamiarisoa@etu.univ-orleans.fr>
2019-04-12 23:37:26 +0200

    ajout de fonction pour la connexité et de la condition d'arrêt

--- solution.ml
+++ solution.ml
@@ -1,16 +1,23 @@
-open Puzzle;;
-open Coordinate;;
-open List;;
+open Puzzle
+open Coordinate
+open List
 type bridge = { isVertical : bool; isDoubled : bool}
 type cell = Nothing | Island of Puzzle.importance | Bridge of bridge
 type solution = cell list list
+type direction = Gauche | Haut | Droite | Bas
 let string_of_bridge b =
   match (b.isDoubled),(b.isVertical) with
   | (false,false) -> "---------"
   | (false,true)  -> "[   |   ]"
   | (true,false)  -> "========="
   | (true,true)   -> "[  | |  ]"
-    
+
+let string_of_pair =
+  function
+  | (x,y) -> ("("^(string_of_int(x)) ^ "," ^ (string_of_int(y))^")")
+           
+let print_pair pair= print_string (string_of_pair pair)
+
 let string_of_cell = function
   | Nothing -> "[       ]"
   | Island x -> ("[Île - " ^ (string_of_int (int_of_importance x))) ^ "]"
@@ -44,6 +51,9 @@ let oob puz =
   let mxR = (Puzzle.getMaxRow puz) in
   function
   | (x,y) -> (x >mxR) || (x < 0) || (y > mxC) || (y < 0);;
+
+
+
 
 
 let init_solution = fun p ->
@@ -83,21 +93,31 @@ let sol1 =
   let bvd = Bridge {isVertical = true;isDoubled = true} in
   let bhs = Bridge {isVertical = false;isDoubled = false} in
   let bhd = Bridge {isVertical = false;isDoubled = true} in[
-    [isl 4;bhd;isl 5;Nothing;Nothing];
-    [bvd;Nothing;bvs;Nothing;Nothing];
-    [isl 3;bhs;isl 3;bhs;isl 1];
-    [Nothing;Nothing;Nothing;Nothing;Nothing];
-    [isl 4;Nothing;isl 8;Nothing;Island (importance_of_int 4)]
+    [isl 4;bhd;isl 4;Nothing;Nothing];
+    [bvd;Nothing;bvd;Nothing;Nothing];
+    [isl 3;bhs;isl 5;bhs;isl 2];
+    [Nothing;Nothing;bvs;Nothing;bvs];
+    [Nothing;Nothing;isl 1;Nothing;isl 1]
   ]
-
+let _ = print_string("SOL 1 = \n"^toString sol1^"\n")
+let coimp c n =
+  let imp n = importance_of_int n in
+  let cfp c = coord_from_pair c in
+  (cfp c,imp n)
 let puz1 =
-  Puzzle.puzzle_of_list ([(coord_from_pair (2,2), importance_of_int 4);(coord_from_pair (4,4),importance_of_int 4)])         
+  Puzzle.puzzle_of_list
+    ([
+        (coimp(4,2) 1);(coimp(2,4) 2);(coimp(2,0) 3);(coimp(0,2) 4);(coimp(0,0) 4);(coord_from_pair (2,2), importance_of_int 5);(coord_from_pair (4,4),importance_of_int 1)])         
 
-type direction = Gauche | Haut | Droite | Bas
+  let _ = print_string("TESTNUL= \n"^toString(init_solution (puzzle_of_list[(coimp(0,0) 4);(coimp (2,2) 8);(coimp(4,2) 1);(coimp(2,4) 2);(coimp(2,0) 3);(coimp(0,2) 4)])))
+  let _ =  print_string("SOL PUZ1 = \n"^toString (init_solution puz1)^"\n")
+let getCell sol = function | (x,y) -> if(oob puz1 (x,y))then failwith"OULAH" else nth (nth sol x) y
+  
+
                                         
 let c = coord_from_pair (1,1)
       
-let getCell sol = function | (x,y) -> if(oob puz1 (x,y))then failwith"OULAH" else nth (nth sol x) y
+
                                         
 let msgFinDebug = ("\n ---------FIN DEBUG----------- \n")
 
@@ -137,9 +157,7 @@ let replace sol pair cell =
   List.rev (aux1 l c v [])
 
 
-let string_of_pair = function
-  | (x,y) -> ("("^(string_of_int(x)) ^ "," ^ (string_of_int(y))^")")
-let print_pair pair= print_string (string_of_pair pair)
+
     
 exception OutOfBounds
 exception IslandMet
@@ -180,8 +198,23 @@ let dessinerPonts sol pair dir =
                   
 let sol2 = dessinerPonts sol1 (2,4) Gauche
          
-
-              
+(* puz1here *)
+let nombre_de_pont sol pair =
+  let aux dir =
+    let bon_sens =
+    function
+    | Bridge {isVertical = true; isDoubled = _ },(Haut|Bas) -> true
+    | Bridge {isVertical = false; isDoubled = _ },(Gauche|Droite) -> true
+    | _,_ -> false in
+    
+    let pair' = next_pair dir pair in
+    if(oob puz1 pair') then 0
+    else
+      let cell = getCell sol pair' in
+      match cell with
+      |(Bridge {isVertical = _; isDoubled = double}) as bridge -> if (bon_sens (bridge,dir)) then (if (double) then 2 else 1) else 0
+      | _ -> 0                                             
+  in (aux Gauche)+(aux Haut)+(aux Droite)+(aux Bas)
 
 let count_total_ponts = fun cell -> fun sol ->
   let haut = getCell sol (fstcoord c - 1, sndcoord c) in
@@ -190,8 +223,8 @@ let count_total_ponts = fun cell -> fun sol ->
   let droite = getCell sol (fstcoord c, sndcoord c + 1) in
   let countPonts = fun c ->
     match c with
-    | Bridge {isVertical = _; isDoubled = b} -> if b then 2 else 1
-    | Nothing -> 0
+    | Bridge {isVertical = _; isDoubled = b} -> if b then 10 else 1
+    | Nothing -> 100
     | _ -> failwith "pas un pont" in
   let nbHaut = countPonts haut in
   let nbBas = countPonts bas in
@@ -205,7 +238,7 @@ let est_complet = fun c -> fun sol ->
     match cell with
     | Island a -> int_of_importance a
     | _ -> failwith "Pas une ile !" in
-  let totalPont = count_total_ponts c sol in 
+  let totalPont = nombre_de_pont sol (pair_from_coord c) in 
   if totalPont < importance then false
   else if importance = totalPont then true
   else failwith "Trop de ponts !"
@@ -301,11 +334,32 @@ let parcours_largeur_pont sol pair =
     let file' = (function |[] -> [] |h::t->t) (file@voisins_non_atteint) in
     match file' with
     |[] -> res
-    |h::t -> aux h (print_string("H = "^(string_of_pair h)^" : "^(string_of_list string_of_pair (t@voisins_non_atteint))^"\n");(file')) (res@voisins_non_atteint)
+    |h::t -> aux h (file') (res@voisins_non_atteint)
   in aux pair [pair] [pair]
 
+
+   
+(* la liste de paire donnée en paramètre doit être issue d'un parcours en largeur*)    
+let test_est_composante_connexe liste sol =
+  (List.length (List.filter (fun x -> est_complet (coord_from_pair x) sol) liste)) = (List.length liste)
+let jeu_est_fini sol puz =
+  let puz' = list_of_puzzle puz in 
+  let liste_finale =
+    let first_pair = pair_from_coord(fst ((function |[]->failwith"puzzle vide"|h::t->h) puz')) in
+
+    parcours_largeur_pont sol first_pair in
+  ((List.length liste_finale) = (List.length puz')) && (test_est_composante_connexe liste_finale sol)
+    
+let _ =
+  let pair = (2,2) in
+  let cell = getCell sol1 pair in
+  let nbPont = string_of_int (nombre_de_pont sol1 pair) in
+  print_string("PAIRE ="^(string_of_pair pair)^" : "^(string_of_cell cell)^", nombre de pont = "^nbPont^"\n")  
+
+let arb_rec = parcours_largeur_pont sol1 (2,2)
+let _ = print_string ((string_of_bool (jeu_est_fini sol1 puz1))^"\n")
 let sol3 = replace sol1 (1,2) (isl 6)
-let msgDebug = "\n"^(toString sol3)^"\n"^(string_of_list (string_of_pair ) (parcours_largeur_pont sol1 (0,0)))^"\n"^(toString sol1) 
+let msgDebug = "\n"^(string_of_list (string_of_pair ) (parcours_largeur_pont sol1 (0,0)))^"\n"^(toString sol1) 
              
 let debugPont = msgDebug^msgFinDebug
 
